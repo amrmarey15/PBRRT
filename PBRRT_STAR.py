96bbc24505eb96b1de50fe01c7933ed5f6c1c339
Amr Marey <amarey@ualberta.ca>
2025-11-04 18:35:11 -0700

    Work_Nov4

--- PBRRT_STAR.py
+++ PBRRT_STAR.py
@@ -1,8 +1,10 @@
 import copy
+import numpy as np
 class PBRRT_STAR:
-    def __init__(self, RRT_params: dict, map: dict, estimator_params: dict):
-        self.RRT_params = copy.deepcopy(RRT_params) # copy.deepcopy points to a whole new dictionary that is identical to the original dictionary
+    def __init__(self, PBRRT_params: dict, map: dict, estimator_params: dict):
+        self.PBRRT_params = copy.deepcopy(PBRRT_params) # copy.deepcopy points to a whole new dictionary that is identical to the original dictionary
         self.map = copy.deepcopy(map)
         self.estimator_params = copy.deepcopy(estimator_params)
-        
+        self.nodes_in_tree = np.ndarray(shape=(1,2))
+        self
         
